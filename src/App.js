c9eb872f55cab7e4c4a4c423e79ec41d73fd0a19
anlunjiang <anlunjiang@outlook.com>
2021-12-21 21:52:12 +0000

    add error modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import AddItem from "./components/shop_list/AddItem";
 import ItemList from "./components/shop_list/ItemList";
-import {useState} from "react";
+import { useState } from "react";
 
 /**
  * Root element to render all other tags
@@ -11,36 +11,34 @@ const App = () => {
   const [itemList, setItemList] = useState([]);
   // handlers
   const addItemHandler = (itemName) => {
-    setItemList(prevItemList => {
-        return [
-          ...prevItemList,
-          itemName,
-        ];
-      }
-    );
+    setItemList((prevItemList) => {
+      return [...prevItemList, itemName];
+    });
   };
   const deleteItemHandler = (itemId) => {
-    setItemList(prevItemList => {
-      return prevItemList.filter(item => item.id !== itemId)
-    })
-  }
+    setItemList((prevItemList) => {
+      return prevItemList.filter((item) => item.id !== itemId);
+    });
+  };
 
   let content = (
-    <p style={{ textAlign: "center" }}>No Items found. Did we buy everything?!</p>
+    <p style={{ textAlign: "center" }}>
+      No Items found. Did we buy everything?!
+    </p>
   );
 
   if (itemList.length > 0) {
-    content = <ItemList itemsList={itemList} onDeleteItem={deleteItemHandler}/>
+    content = (
+      <ItemList itemsList={itemList} onDeleteItem={deleteItemHandler} />
+    );
   }
 
   return (
     <div>
-      <AddItem onAddItem={addItemHandler}/>
-      <section id="items">
-        {content}
-      </section>
+      <AddItem onAddItem={addItemHandler} />
+      <section id="items">{content}</section>
     </div>
-  )
-}
+  );
+};
 
 export default App;
